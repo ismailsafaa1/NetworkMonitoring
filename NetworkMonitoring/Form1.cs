473690006d3ba5f82d75114b28eb39d7f79a1777
ismailsafaa1 <ismaeltech3@gmail.com>
2025-07-30 18:44:18 +0300

    bnbnbnbnbnb

--- NetworkMonitoring/Form1.cs
+++ NetworkMonitoring/Form1.cs
@@ -16,7 +16,10 @@ namespace NetworkMonitoring
         {
             InitializeComponent();
         }
-        
+        public void LoadData()
+        {
+            string name = "jjj";
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             picAP.BackColor = Color.Green;
